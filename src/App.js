ac5103d4951606435c2003c887ab61dba712c4b5
Zeeshan Ali <zee03044163645@gmail.com>
2022-10-24 21:16:49 +0500

    table component and add typescript in packages.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ListComp from './components/ListComp';
 import AvaterComp from './components/AvaterComp';
 import { Box } from '@mui/system';
 import RadioButton from './components/RadioButton';
+import TableComp from './components/TableComp';
 
 function App() {
 
@@ -135,6 +136,8 @@ function App() {
       <AvaterComp />
       <hr />
       <RadioButton />
+      <hr />
+      <TableComp />
     </div>
   );
 }
